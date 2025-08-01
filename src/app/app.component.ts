5a54324c693a75da5f2b7fa3dbf02a330ba9900b
teoto4 <teoto@ukr.net>
2025-08-01 23:36:38 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'test';
+
+
+
+  generatePassword(){
+    console.log("generate");
+    
+  }
+
+  constructor(){}
 }
